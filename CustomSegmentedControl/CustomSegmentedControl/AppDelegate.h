51b327d9ed06a0e665e17c3067eaf2110601bf6e
skor33393 <skor33393@yandex.ru>
2016-08-22 21:11:52 +0300

    initial

---
+++ CustomSegmentedControl/CustomSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSegmentedControl
+//
+//  Created by Alexander Korenev on 22.08.16.
+//  Copyright © 2016 Sphere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomSegmentedControl/CustomSegmentedControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomSegmentedControl
+//
+//  Created by Alexander Korenev on 22.08.16.
+//  Copyright © 2016 Sphere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
